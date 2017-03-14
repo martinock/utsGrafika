4707fb35a286aa7f4bf5048e1acfdb577ecc1fd9
martinock <martino.aksel.11@gmail.com>
2017-03-14 10:12:51 +0700

    Fixed rotation scale

--- ITBMapView/rotate.c
+++ ITBMapView/rotate.c
@@ -1,18 +1,18 @@
 #include "grafika.h"
 
-#define PI 3.14159265
+#define PI acos(-1)
 
 
 Point rotatePoint(Point p ,Point pivot, float angle){
-	float s = sin(angle * PI / 180);
-	float c = cos(angle * PI /180);
+	float s = sin(angle * PI / 180.0);
+	float c = cos(angle * PI / 180.0);
 	p.x -= pivot.x;
 	p.y -= pivot.y;
 
-	int xnew = (int) (p.x * c - p.y *s);
-	int ynew = (int) (p.x * s + p.y *c);
+	int xnew = (int) (p.x * c - p.y * s);
+	int ynew = (int) (p.x * s + p.y * c);
 
-	p.x  = xnew + pivot.x;
+	p.x = xnew + pivot.x;
 	p.y = ynew + pivot.y;
 	return p;
 
@@ -22,7 +22,7 @@ Point rotatePoint(Point p ,Point pivot, float angle){
 Point* rotateMany(Point p, Point* p1, double angle, int length) {
   Point *temp = (Point*) malloc(length*sizeof(Point));
   for (int i = 0; i < length; i++) {
-    temp[i] = rotatePoint(p, p1[i], angle);
+    temp[i] = rotatePoint(p1[i], p, angle);
   }
   return temp;
 }
